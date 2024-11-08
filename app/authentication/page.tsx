fd3e9fc781d49173e73c2bb9471b881fe0a17723
Asolinmaso <asolinmaso22@gmail.com>
2024-11-08 12:27:12 +0000

    Address

--- app/authentication/page.tsx
+++ app/authentication/page.tsx
@@ -1,7 +1,20 @@
+"use client"
 import CommonNavBar from "@/components/Molecules/NavBar"
 import AuthenticationComponent from "@/components/Organelles/Authentication"
+import { useAuth } from "@/components/Wrapper/universalState";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function AuthenticationLoginPage () {
+    const router = useRouter();
+  const { selectedUserData } = useAuth();
+  useEffect(() => {
+    if (selectedUserData?.email) {
+      router.push("/");
+    }
+  }, [selectedUserData, router]);
+
+
     return (
         <>
         <CommonNavBar />
